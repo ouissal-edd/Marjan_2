4db770fc894bcd4affa501e6a51cb35611d09f6d
ouissal-edd <ouissal.edouj21@gmail.com>
2022-01-10 15:09:59 +0100

    Marjan2

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const router = require('./router/router');
 const bodyParser = require('body-parser');
 
+const session = require('express-session');
+const cookieParser = require('cookie-parser')
+
 
 
 // static files
@@ -23,26 +26,46 @@ app.use(bodyParser.urlencoded({
 app.get('/pdg', function (req, res) {
     res.render('pdg/login');
 });
-app.get('/g_promos', function (req, res) {
-    res.render('pdg/gestion_promos');
-});
+
 app.get('/g_account', function (req, res) {
-    res.render('pdg/gestion_account');
+    res.redirect('/getCenter');
 });
 app.get('/admin', function (req, res) {
     res.render('admin/dashAdmin');
 });
+app.get('/admin/login', function (req, res) {
+    res.render('admin/logiin');
+});
+
+app.get('/g_account_admin', function (req, res) {
+    res.redirect('/getChef');
+});
+// app.get('/g_prom', function (req, res) {
+//     res.redirect('admin/gestion_account');
+// });
+
 
 
 
 
 
 // action
+//pdg
 app.post('/loginPdg', router.loginPdg);
 app.get('/pdgDash', router.getDataToPdg);
 app.post('/addCenterAdmin', router.addAdminCentre);
+app.get('/getCenter', router.getCenter);
 app.post('/deletePromo/:id', router.deletePromotion);
 app.post('/addCategorie', router.addCategorie);
+
+// admin
+app.post('/loginAdmin', router.loginAdmin);
+app.get('/getChef', router.getChefRayon);
+app.post('/addPromotion', router.addPromotion);
+app.post('/addChefR', router.addChefRayon);
+
+
+
 
 
 
--- router/router.js
+++ router/router.js
@@ -9,22 +9,154 @@ exports.loginPdg = (req, rese) => {
             "email_pdg": req.body.email_pdg,
             "password_pdg": req.body.password_pdg
         })
-        .then((res) => {
-            if (res.data.success == 0) {
-                res.render('login');
+        .then((res3) => {
+            if (res3.data.success == 0) {
+                rese.render('pdg/login');
             } else {
-                rese.redirect('/pdgDash')
+                axios.get('http://localhost:3000/api/pdg/statistics')
+                    .then((res1) => {
+                        axios.get('http://localhost:3000/api/pdg/promos')
+
+                            .then(res2 => (console.log(res3.data.pdg), rese.render('pdg/pdgm', {
+                                statestics: res1.data,
+                                allPromo: res2.data.data,
+                                Jwt: res3.data.token,
+                                numCenter: res3.data.pdg,
+                            })))
+
+
+                            .catch(err2 => (console.log(err2)));
+                    })
+                    .catch(err => (console.log(err)))
+            }
+        })
+        .catch(err => (console.log(err)));
+
+},
+exports.loginAdmin = (req, rese) => {
+    axios.post('http://localhost:3000/api/admin/loginAdmin', {
+
+        "email_admin": req.body.email_admin,
+        "password_admin": req.body.password_admin
+        })
+        .then((res3) => {
+            if (res3.data.success == 0) {
+                rese.render('admin/logiin');
+            } else {
+                axios.get('http://localhost:3000/api/admin/product')
+                    .then((res1) => {
+                        axios.post('http://localhost:3000/api/admin/getPromoByCenter',{"centre":res3.data.admin})
+                        .then((res6) => {
+                        axios.post('http://localhost:3000/api/admin/getAdminRayon',{"centre":res3.data.admin})
+                            .then(res2 => ( rese.render('admin/dashAdmin', {
+                                products: res1.data.data,
+                                adminRayon: res2.data.data,
+                                Jwt: res3.data.token,
+                                promo: res6.data.data,
+                                num_centre:res3.data.admin
+                            })))
+
+
+                            .catch(err2 => (console.log(err2)));
+                })
+                .catch(err6 => (console.log(err6)))
+                    })
+                    .catch(err1 => (console.log(err1)))
             }
         })
         .catch(err => (console.log(err)));
 
 }
 
+// exports.loginAdmin = (req, rese) => {
+//     axios.post('http://localhost:3000/api/admin/loginAdmin', {
+
+//             "email_admin": req.body.email_admin,
+//             "password_admin": req.body.password_admin
+//         })
+//         .then((res) => {
+//             if (res.data.success == 0) {
+//                 rese.render('admin/logiin');
+//             } else {
+//                 rese.redirect('/getData')
+//             }
+//         })
+//         .catch(err => (console.log(err)));
+
+// }
+
+exports.getCenter = (req, resu) => {
+    axios.get('http://localhost:3000/api/pdg/centre')
+        .then((res) => {
+            axios.get('http://localhost:3000/api/pdg/centrAdmin')
+                .then(res2 => ( resu.render('pdg/gestion_account', {
+                    center: res.data.data,
+                    adminCentre: res2.data.data
+                })))
+                .catch(err2 => (console.log(err2)));
+        })
+        .catch(err => (console.log(err)))
+  
+
+}
+
+exports.getChefRayon = (req, resu) => {
+    const cc= req.body.centree;
+    axios.get('http://localhost:3000/api/admin/category')
+    .then((res) => {
+    axios.post('http://localhost:3000/api/admin/getAdminRayon',{"centre":1})
+                .then(res2 => ( resu.render('admin/gestion_account', {
+                  adminRayon: res2.data.data,
+                  categorie: res.data.data,
+
+                })))
+                .catch(err2 => (console.log(err2)));
+            })
+            .catch(err => (console.log(err)))
+}
+exports.addChefRayon = (req, rese) => {
+
+    axios.post('http://localhost:3000/api/admin/rayonUser', {
+
+            "nom_admin_rayon": req.body.nom_admin_rayon,
+            "email_admin_rayon": req.body.email_admin_rayon,
+            "password_admin_rayon": req.body.password_admin_rayon,
+            "fk_centre": req.body.center,
+            "fk_cat":req.body.categorie,
+
+        })
+        .then(res => (rese.redirect('/getChef')))
+        .catch(err => (console.log(err)));
+}
+
+
+
+// exports.getData = (req, resu) => {
+
+//     axios.get('http://localhost:3000/api/admin/product')
+//         .then((res) => {
+//             axios.post('http://localhost:3000/api/admin/getAdminRayon', {
+//                     "centre": 1,
+
+//                 })
+//                 .then(res2 => (resu.render('admin/dashAdmin', {
+//                     products: res.data.data,
+//                     adminRayon: res2.data.data,
+//                 })))
+
+
+//                 .catch(err2 => (console.log(err2)));
+//         })
+//         .catch(err => (console.log(err)))
+
+// }
+
+
 exports.getDataToPdg = (req, resu) => {
     axios.get('http://localhost:3000/api/pdg/statistics')
         .then((res) => {
             axios.get('http://localhost:3000/api/pdg/promos')
-                .then(res2 => (console.log(res, ), resu.render('pdg/pdgm', {
+                .then(res2 => (resu.render('pdg/pdgm', {
                     statestics: res.data,
                     allPromo: res2.data.data
                 })))
@@ -45,7 +177,7 @@ exports.addAdminCentre = (req, rese) => {
             "fkcentre": req.body.center
 
         })
-        .then(res => (rese.redirect('pdg/gestion_account')))
+        .then(res => (rese.render('pdg/gestion_account')))
         .catch(err => (console.log(err)));
 }
 exports.deletePromotion = (req, resu) => {
@@ -56,11 +188,30 @@ exports.deletePromotion = (req, resu) => {
 exports.addCategorie = (req, rese) => {
 
     axios.post('http://localhost:3000/api/pdg/createCategorie', {
-
             "type_cat": req.body.type_cat,
+        })
+        .then(res => (rese.render('pdg/gestion_account')))
+        .catch(err => (console.log(err)));
+}
 
+exports.addPromotion = (req, rese) => {
+    axios.post('http://localhost:3000/api/admin/promo', {
+
+            "date_debut": req.body.date_debut,
+            "date_fin": req.body.date_fin,
+            "fk_rayon": req.body.fk_rayon,
+            "fk_prod": req.body.prooduit,
+            "remise": req.body.remise,
+            "fk_admin": req.body.fk_admin,
 
         })
-        .then(res => (rese.redirect('pdg/gestion_account')))
+        .then((res) => {
+            if (res.data.success == 0) {
+                res.render('login');
+            } else {
+                rese.redirect('/getData')
+            }
+        })
         .catch(err => (console.log(err)));
+
 }
